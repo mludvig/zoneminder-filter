44778d5f22543355eea9f7c0f538539c23492d5a
Michael Ludvig <mludvig@logix.net.nz>
2018-12-30 07:58:31 +1300

    Simple ZoneMinder event checker
    Delete events that don't capture any interesting objects (Car, Person, ...)

---
+++ rekognition.py
@@ -0,0 +1,71 @@
+import io
+import boto3
+from PIL import Image
+
+class RekognitionHelper():
+    def __init__(self, size):
+        self.client = boto3.client('rekognition')
+        self.size = size
+
+    def get_labels(self, file_name, only_labels = [], with_instances = True, min_confidence = 90):
+        image = Image.open(file_name)
+
+        thumb = image.copy()
+        thumb.thumbnail(self.size, Image.ANTIALIAS)
+
+        thumb_io = io.BytesIO()
+        thumb.save(thumb_io, 'JPEG')
+        thumb_io.seek(0)
+
+        response = self.client.detect_labels(
+            Image={
+                'Bytes': thumb_io.read()
+            },
+            MaxLabels=10,
+            MinConfidence=min_confidence,
+        )
+
+        labels = response['Labels']
+
+        if only_labels:
+            labels = [label for label in labels if label['Name'] in only_labels]
+
+        if with_instances:
+            labels = [label for label in labels if label['Instances']]
+
+        return labels
+
+    def _tmp_get_boxes():
+        for label in response['Labels']:
+            if not label['Instances']:
+                continue
+            print("Label: " + label['Name'])
+            print("Confidence: " + str(label['Confidence']))
+            print("Instances:")
+            for instance in label['Instances']:
+                #print("  Bounding box")
+                #print("    Top: " + str(instance['BoundingBox']['Top']))
+                #print("    Left: " + str(instance['BoundingBox']['Left']))
+                #print("    Width: " +  str(instance['BoundingBox']['Width']))
+                #print("    Height: " +  str(instance['BoundingBox']['Height']))
+                print("  Confidence: " + str(instance['Confidence']))
+                print()
+
+                crop_pix = (
+                    im.width * instance['BoundingBox']['Left'],
+                    im.height * instance['BoundingBox']['Top'],
+                    im.width * (instance['BoundingBox']['Left'] + instance['BoundingBox']['Width']),
+                    im.height * (instance['BoundingBox']['Top'] + instance['BoundingBox']['Height'])
+                )
+                im_crop = im.crop(crop_pix)
+                im_pyg = pygame.image.fromstring(im_crop.tobytes(), im_crop.size, im_crop.mode)
+                display.fill(display_bgcolour)
+                display.blit(im_pyg, (
+                    (display_width - im_crop.width) / 2,
+                    (display_height - im_crop.height) / 2
+                ))
+                pygame.display.flip()
+                time.sleep(2)
+
+            print("----------")
+            print()
---
+++ zm-list.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3.6
+
+import os
+import glob
+import json
+import requests
+
+from rekognition import RekognitionHelper
+
+class ZmApi():
+    def __init__(self, api_base_url):
+        self.base = api_base_url
+
+    def get_index(self, monitor_id):
+        url = "%s/events/index/MonitorId:%d.json" % (self.base, monitor_id)
+        r = requests.get(url, verify=False)
+        return json.loads(r.text)
+
+    def get_event(self, event_id):
+        url = "%s/events/%s.json" % (self.base, event_id)
+        r = requests.get(url, verify=False)
+        return json.loads(r.text)
+
+    def delete_event(self, event_id):
+        url = "%s/events/%s.json" % (self.base, event_id)
+        r = requests.delete(url, verify=False)
+
+class ZmFiles():
+    def __init__(self, base_dir, zm_api):
+        self.dir = base_dir
+        self.zm_api = zm_api
+
+    def frames(self, event = None, event_id = 0, stride = 50):
+        frames = []
+        if event is None:
+            event = self.zm_api.get_event(event_id)
+            event = event['event']['Event']
+        base_dir = os.path.join(self.dir, event['BasePath'])
+        idx = 0
+        try:
+            dirlist = glob.glob(os.path.join(base_dir, "*-capture.jpg"))
+            dirlist.sort()
+        except FileNotFoundError:
+            return frames
+        while True:
+            try:
+                frames.append(os.path.join(base_dir, dirlist[idx]))
+                idx += stride
+            except IndexError:
+                break
+        return frames
+
+if __name__ == "__main__":
+    zma = ZmApi("https://172.31.174.9/zm/api")
+    zmf = ZmFiles("/var/lib/zoneminder", zma)
+    rek = RekognitionHelper(size = (800,800))
+
+    idx = zma.get_index(monitor_id = 4)
+    event_id = idx['events'][0]['Event']['Id']
+    while event_id:
+        delete = True
+        event = zma.get_event(event_id)['event']['Event']
+        print("%(Id)s - %(StartTime)s - %(Frames)-6s" % event)
+        frames = zmf.frames(event = event)
+        for frame in frames:
+            labels = rek.get_labels(frame)
+            labels_names = ",".join([label['Name'] for label in labels])
+            print("    %s  %s" % (frame, labels_names or "-"), flush = True)
+            if labels:
+                delete = False
+                print("    # skipping further checks")
+                break
+        if delete:
+            print("DELETING %(Id)s" % event)
+            zma.delete_event("%(Id)s" % event)
+        else:
+            print("RETAINING %(Id)s" % event)
+        print()
+        event_id = event['NextOfMonitor']
